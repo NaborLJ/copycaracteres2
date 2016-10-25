b958f4fcfeada533d915a70917417247a9028f51
oracle <oracle@localhost>
2016-10-25 09:34:17 +0200

    xx

---
+++ src/copycaracteres2/Copycaracteres2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package copycaracteres2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author oracle
+ */
+public class Copycaracteres2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        File texto = new File("texto11.txt");
+        texto.createNewFile();
+        
+        BufferedReader read = new BufferedReader(new FileReader("texto10.txt"));
+        PrintWriter write = new PrintWriter("texto11.txt");
+        String aux="";
+        for(int i = 0;i<3;i++){
+        aux=read.readLine();
+        write.println(aux);
+        }
+        read.close();
+        write.close();
+
+    }
+    
+}
